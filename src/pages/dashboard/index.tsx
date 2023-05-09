c0731d75e80c429489de120d5cf4414f8241c62d
Pablo Leite <pabloleite03@gmail.com>
2023-05-08 21:23:46 -0300

    Add files via upload

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from 'next';
 import styles from './styles.module.css';
 import Head from 'next/head';
+import { getSession } from 'next-auth/react';
 
 export default function Dashboard() {
   return (
@@ -11,4 +13,22 @@ export default function Dashboard() {
       <h1>Painel de tarefas</h1>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+  //console.log(session);
+
+  if(!session?.user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {},
+  }
 }
